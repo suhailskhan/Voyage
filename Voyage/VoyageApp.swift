8c9b4b77f950213520b1f1a5c79c3309b55d46a9
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2024-02-20 06:58:44 -0800

    Initial Commit

---
+++ Voyage/VoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoyageApp.swift
+//  Voyage
+//
+//  Created by Suhail Khan on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct VoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
